72766da0c85625b82177ca9c28cab02eaddc1daa
Viktor Gavrilenko <viktargavrilenko@gmail.com>
2021-10-15 09:38:33 +0300

    Внес изменения в метод базовой авторизации

--- src/main/java/com/example/page_object/BasicAuthForm.java
+++ src/main/java/com/example/page_object/BasicAuthForm.java
@@ -1,18 +1,13 @@
 package com.example.page_object;
 
-import com.example.config.ConfigurationProperties;
 import com.example.core.DriverUtilities;
 import com.example.elements.Form;
 import com.example.elements.TextField;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 
-import java.util.Properties;
-
 public class BasicAuthForm extends BaseForm {
     private static final Logger LOG = Logger.getLogger(BasicAuthForm.class);
-    private static final Properties testDataProperties =
-            ConfigurationProperties.createProperties("testData.properties");
     private static final Form FORM = new Form(By.xpath("//div[@id='content']"), "BasicAuthForm");
     private static final TextField TEXT_AUTHORIZED = new TextField(By.xpath("//div[@class='example']//p"),
             "BasicAuthText");
@@ -25,12 +20,10 @@ public class BasicAuthForm extends BaseForm {
         return TEXT_AUTHORIZED.getText();
     }
 
-    public void userAuthorization(String login, String password) {
-        String hostUrl = testDataProperties.getProperty("main.page");
-        String pathUrl = testDataProperties.getProperty("basic.auth.page");
+    public void userAuthorization(String login, String password, String url) {
         String logData = login + ":" + password + "@";
-        hostUrl = new StringBuilder(hostUrl).insert(hostUrl.indexOf("//") + 2, logData).toString();
-        DriverUtilities.goToUrl(hostUrl + pathUrl);
+        url = new StringBuilder(url).insert(url.indexOf("//") + 2, logData).toString();
+        DriverUtilities.goToUrl(url);
         LOG.info("Enter your username and password");
     }
 }
--- src/test/java/com/example/BasicAuthTest.java
+++ src/test/java/com/example/BasicAuthTest.java
@@ -7,15 +7,15 @@ import org.testng.annotations.Test;
 public class BasicAuthTest extends BaseTest {
 
     private static final Logger LOG = Logger.getLogger(BasicAuthTest.class);
-    public static final String TEXT = "Congratulations! You must have the proper credentials.";
+    private static final String TEXT = "Congratulations! You must have the proper credentials.";
 
     @Test(description = "Тест формы авторизации")
     public void testBasicAuth() {
         BasicAuthForm basicAuth = new BasicAuthForm();
-        LOG.info("Navigate to URL " + testDataProperties.getProperty("main.page") +
-                testDataProperties.getProperty("basic.auth.page") + "and pass basic authorization credentials");
+        String url = testDataProperties.getProperty("main.page") + testDataProperties.getProperty("basic.auth.page");
+        LOG.info("Navigate to URL " + url + "and pass basic authorization credentials");
         basicAuth.userAuthorization(testDataProperties.getProperty("login"),
-                testDataProperties.getProperty("password"));
+                testDataProperties.getProperty("password"), url);
         assertEquals(basicAuth.getTextAuthorized(), TEXT, "Проверка авторизации на странице BasicAuth");
     }
 }
